34c3dfe8de2a559091062751700e8c1da743d5d8
Warhawk12121 <mrigankm20@gmail.com>
2021-04-02 02:14:13 +0530

    Easy button can be used only

--- game.py
+++ game.py
@@ -4,9 +4,38 @@ The main game loop.
 Holds the GUI as well as the implementation of the game logic.
 """
 
-from gamestate import GameState
+from gamestate import GameState 
 
-import pygame
+def easy():
+    gs= GameState('EASY')
+    running = True
+    
+    while running:
+
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            screen.fill((0,0,0))
+            if event.type == pygame.QUIT:
+                screen.fill((0,0,0))
+                pygame.quit()
+                sys.exit()
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    
+        Hints = Font.render('Hints:', True, (0, 128, 0))
+        
+        screen.blit(Hints,(0,0))   
+        gs.readyState()
+        textsurface = Font.render('HP', True, (0, 128, 0))
+        HP=Font.render(str(gs.healthPoints), True, (0, 128, 0))
+        screen.blit(HP,(800,10))
+        screen.blit(textsurface,(750,10))   
+        pygame.display.flip() 
+
+
+import pygame,sys
 pygame.init()
 
 # Set the window
@@ -26,52 +55,37 @@ while running:
     # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
+                    running = False
+                    
 
-        mx, my = pygame.mouse.get_pos()
-        #button_1 = pygame.Rect(380, 200, 100, 25)
+        if event.type ==  pygame.MOUSEBUTTONDOWN:
+            if 180 <= mouse[0] <= 180+65 and 200 <= mouse[1] <= 200+20:
+                easy()
+            
+        screen.fill((0,0,0))
+        mouse = pygame.mouse.get_pos()
 
         s_diff=Font.render('Select difficulty', True, (0,255, 0))
         screen.blit(s_diff,(350,150))
 
-        button_1=Font.render('Easy', True, (255,255, 0),(0, 0, 128))
+        button_1=Font.render('EASY', True, (255,255, 0),(0, 0, 128))
         screen.blit(button_1,(180,200))
 
-        button_2=Font.render('Medium', True, (255,255, 0),(0, 0, 128))
+        button_2=Font.render('MEDIUM', True, (255,255, 0),(0, 0, 128))
         screen.blit(button_2,(380,200))
 
-        button_3=Font.render('Hard', True, (255,255, 0),(0, 0, 128))
+        button_3=Font.render('HARD', True, (255,255, 0),(0, 0, 128))
         screen.blit(button_3,(600,200))
 
-        '''
-        if pygame.mouse.get_pressed()[0] and button_1.collidepoint((mx,my)):
-            h_game()
-        '''
-        #pygame.draw.rect(screen,(0,0,255),button_1)
-
-        Health = Font.render('Main Menu', True, (255, 0, 0))
-        screen.blit(Health,((width//2)-50,50))
+        
+        
+        menu = Font.render('Main Menu', True, (255, 0, 0))
+        screen.blit(menu,((width//2)-50,50))
           
         pygame.display.update()
 
 
-def h_game():
-    running = True
-    while running:
-
-        # Did the user click the window close button?
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-        
-        textsurface = Font.render('Health bar', False, (0, 128, 0))
-        screen.blit(textsurface,(390,10))   
-        pygame.display.flip() 
